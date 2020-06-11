2ae8ed9c65585d86d6945ed2a9c60896c0d181d6
Kiminuo <kiminuo@protonmail.com>
2020-06-11 11:12:59 +0000

    walletutil: Replace `level()` with `depth()`
    (cherry picked from commit 06877d38273fe5cadb014231c40dd170a4a5cc4b)
    (cherry picked from commit a79d02ac67e2be27d6c845eb6cb2c68a651f46a9)

--- src/wallet/walletutil.cpp
+++ src/wallet/walletutil.cpp
@@ -74,7 +74,7 @@ std::vector<fs::path> ListWalletDir()
         if (it->status().type() == fs::file_type::directory && IsBerkeleyBtree(it->path() / "wallet.dat")) {
             // Found a directory which contains wallet.dat btree file, add it as a wallet.
             paths.emplace_back(path);
-        } else if (it.level() == 0 && it->symlink_status().type() == fs::file_type::regular && IsBerkeleyBtree(it->path())) {
+        } else if (it.depth() == 0 && it->symlink_status().type() == fs::file_type::regular && IsBerkeleyBtree(it->path())) {
             if (it->path().filename() == "wallet.dat") {
                 // Found top-level wallet.dat btree file, add top level directory ""
                 // as a wallet.
