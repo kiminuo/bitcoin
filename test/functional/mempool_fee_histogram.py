7b6da9fa33c94553436721d39634b53b706a1e72
Kiminuo <kiminuo@protonmail.com>
2021-03-06 22:54:05 +0100

    test: Add mempool fee histogram test coverage
    Original commit: https://github.com/bitcoin/bitcoin/commit/0b6ba66238c377116bc6c21e19cffbf1b6dfc788
    
    Co-authored-by: João Barbosa <joao.paulo.barbosa@gmail.com>
    Co-authored-by: Jon Atack <jon@atack.com>

---
+++ test/functional/mempool_fee_histogram.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# Copyright (c) 2021 The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+"""Test mempool fee histogram."""
+
+from test_framework.test_framework import BitcoinTestFramework
+from test_framework.util import (
+    assert_equal,
+    assert_greater_than,
+    assert_greater_than_or_equal,
+    assert_no_key,
+)
+
+class MempoolFeeHistogramTest(BitcoinTestFramework):
+    def set_test_params(self):
+        self.num_nodes = 1
+
+    def skip_test_if_missing_module(self):
+        self.skip_if_no_wallet()
+
+    def run_test(self):
+        node = self.nodes[0]
+
+        node.sendtoaddress(node.getnewaddress(), 1)
+
+        info = node.getmempoolinfo()
+        assert_no_key('fee_histogram', info)
+
+        info = node.getmempoolinfo([100, 200, 300, 400, 500])
+        total_fees = 0
+
+        for key, bin in info['fee_histogram']['fee_rate_groups'].items():
+            assert_equal(int(key), bin['from'])
+            if bin['fees'] > 0:
+                assert_greater_than(bin['count'], 0)
+            else:
+                assert_equal(bin['count'], 0)
+            assert_greater_than_or_equal(bin['fees'], 0)
+            assert_greater_than_or_equal(bin['size'], 0)
+            if bin['to'] != 'max':
+                assert_greater_than(bin['to'], bin['from'])
+            total_fees += bin['fees']
+
+        assert_equal(total_fees, info['fee_histogram']['total_fees'])
+
+
+if __name__ == '__main__':
+    MempoolFeeHistogramTest().main()
